dbbd8b35d274bb05bd24eaae6f1025a47401046a
Bogdanov-Tsabert Timogor <mr.timogor@mail.ru>
2025-02-15 03:28:12 +0500

    added some URLs in Constant

--- src/test/java/util/Constant.java
+++ src/test/java/util/Constant.java
@@ -2,4 +2,9 @@ package util;
 
 public class Constant {
     public static final String TEST_URI = "https://qa-scooter.praktikum-services.ru/";
+    public static final String COURIER_CREATE_TEST_URL = TEST_URI + "api/v1/courier";
+    public static final String COURIER_LOGIN_TEST_URL = TEST_URI + "api/v1/courier/login";
+    public static final String ORDER_CREATE_TEST_URL = TEST_URI + "api/v1/orders";
+    public static final String ORDERS_LIST_GET_TEST_URL = TEST_URI + "api/v1/orders";
+    public static final String ORDER_CANCEL_TEST_URL = TEST_URI + "api/v1/orders/cancel";
 }
