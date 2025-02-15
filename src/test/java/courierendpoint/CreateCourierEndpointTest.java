1c7733aae982f231baba873a129b10466ff1ad9f
Bogdanov-Tsabert Timogor <mr.timogor@mail.ru>
2025-02-15 06:32:19 +0500

    Fix 3

--- src/test/java/courierendpoint/CreateCourierEndpointTest.java
+++ src/test/java/courierendpoint/CreateCourierEndpointTest.java
@@ -3,24 +3,37 @@ package courierendpoint;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
+
 import io.restassured.response.ValidatableResponse;
 
+import net.datafaker.Faker;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import util.client.ScooterServiceClient;
+
 import util.model.Courier;
 import util.model.Credentials;
 
 
+import java.util.Random;
+
 import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.CoreMatchers.equalTo;
+
 
 @DisplayName("Тесты создания курьера")
 public class CreateCourierEndpointTest {
 
     // Для хранения данных последнего использованного курьера
     Courier courier;
+
+    // Для данных курьера
+    private String courierLogin;
+    private String courierPassword;
+    private String courierName;
 
     @After
     public void tearDown(){
@@ -30,11 +43,24 @@ public class CreateCourierEndpointTest {
         client.deleteCourier(response.extract().jsonPath().getString("id"));
     }
 
+    @Before
+    public void setUp(){
+        long seed = new Random().nextLong();
+        Faker faker = new Faker(new Random(seed));
+
+        // Т.к. требований к паролю/логину/имени мы не имеем, сделал так
+        this.courierPassword = faker.lorem().characters(6, 12);
+        this.courierLogin = faker.lorem().characters(12);
+        this.courierName = faker.name().name();
+
+
+    }
+
     @Test
     @DisplayName("Создание курьера")
     @Description("Создание курьера со всеми валидными параметрами")
     public void  createCourierTest(){
-        this.courier = new Courier("timofeevich", "qwerty", "Timofey");
+        this.courier = new Courier(this.courierLogin, this.courierPassword, this.courierName);
         ScooterServiceClient client = new ScooterServiceClient();
 
         ValidatableResponse response = client.createCourier(courier);
@@ -49,7 +75,7 @@ public class CreateCourierEndpointTest {
     @DisplayName("Дублированный логин")
     @Description("Попытка создать курьера с логином, который уже занят")
     public void unavailabilityOfCreatingTwoIdenticalCourierTest(){
-        this.courier = new Courier("timofeevich", "qwerty", "Timofey");
+        this.courier = new Courier(this.courierLogin, this.courierPassword, this.courierName);
 
         ScooterServiceClient client = new ScooterServiceClient();
 
@@ -67,7 +93,7 @@ public class CreateCourierEndpointTest {
     @DisplayName("Отсутствующий логин")
     @Description("Попытка создать курьера с отсутствующим логином в body")
     public void mandatoryOfLoginBodyParameterTest(){
-        this.courier = new Courier("", "qwerty", "Timofey");
+        this.courier = new Courier("", this.courierPassword, this.courierName);
         ScooterServiceClient client = new ScooterServiceClient();
         ValidatableResponse response = client.createCourier(this.courier);
 
@@ -80,7 +106,7 @@ public class CreateCourierEndpointTest {
     @DisplayName("Отсутствующий пароль")
     @Description("Попытка создать курьера с отсутствующим паролем в body")
     public void mandatoryOfPasswordBodyParameterTest(){
-        this.courier = new Courier("timofeevich", "", "Timofey");
+        this.courier = new Courier(this.courierLogin, "", this.courierName);
         ScooterServiceClient client = new ScooterServiceClient();
         ValidatableResponse response = client.createCourier(this.courier);
 
@@ -93,7 +119,7 @@ public class CreateCourierEndpointTest {
     @DisplayName("Отсутствующее имя")
     @Description("Попытка создать курьера с отсутствующим именем в body")
     public void optionalityOfFirstNameBodyParameterTest(){
-        this.courier = new Courier("timofeevich", "qwerty", "");
+        this.courier = new Courier(this.courierLogin, this.courierPassword, "");
         ScooterServiceClient client = new ScooterServiceClient();
 
         ValidatableResponse response = client.createCourier(courier);
